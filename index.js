9bf07ac325694625b60eeec9da848eab5fdf10b4
Pratik <pratik.gharge@31gmail.com>
2022-08-02 18:54:40 +0530

    updated code

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //FOR PRODUCTION
 if(process.env.NODE_ENV=="production"){
-    app.use(express.static("client/build"))
+    app.use(express.static("frontend/build"))
 }
 
 app.listen(process.env.PORT || 8008,()=>{
